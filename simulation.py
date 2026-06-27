a67df65fe1f6701955b4761a08147696a20421c1
Minh <baominh19072009@gmail.com>
2026-06-27 10:06:47 +0700

    creating graphing file only for text

--- simulation.py
+++ simulation.py
@@ -1,3 +1,8 @@
+# chia function collision simulation va detection va fdata analysis
+# function collision run all colllisions and detection function run cutoffs and select events
+# function data analysis reads data file and draw histogram of energy distribution
+# 
+
 import math
 import random
 
